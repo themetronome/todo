61bded77daff588c693b03a073bf164a5c2fd0da
Vladislav Bukatov <vasyayp@yandex.ru>
2021-02-26 17:00:13 +0400

    make base todo components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Test() {
-  return <h1>test!</h1>;
-}
+const AppHeader = () => {
+  return <h1>todo list</h1>;
+};
 
-ReactDOM.render(<Test />, document.getElementById("root"));
+const SearchField = () => {
+  return <input placeholder="search" />;
+};
+
+const TodoListItem = () => {
+  return (
+    <li>
+      <span>learn react</span>
+    </li>
+  );
+};
+
+const TodoList = () => {
+  return (
+    <ul>
+      <TodoListItem />
+      <TodoListItem />
+    </ul>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchField />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
