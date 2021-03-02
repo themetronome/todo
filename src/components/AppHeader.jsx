df41b3adb7b425e6af9220f18c5a4c518c78cfa2
Vladislav Bukatov <vasyayp@yandex.ru>
2021-03-02 18:31:06 +0400

    basic project structure

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,3 @@
+export const AppHeader = () => {
+  return <h1>todo list</h1>;
+};
---
+++ src/components/TodoListItem.jsx
@@ -0,0 +1,7 @@
+export const TodoListItem = () => {
+  return (
+    <li>
+      <span>learn react</span>
+    </li>
+  );
+};
