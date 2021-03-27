756cc814e69abe89c8fc23c67ad9e5aed73e5eda
Vladislav Bukatov <vasyayp@yandex.ru>
2021-03-27 14:24:58 +0400

    chore: add classnames lib and use it in list component

--- src/components/sidebar-list/SidebarList.jsx
+++ src/components/sidebar-list/SidebarList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import classNames from 'classnames'
 
 import './SidebarList.scss'
 
-export const SidebarList = ({ items }) => {
+export const SidebarList = ({ items, isRemovable }) => {
   return (
     <ul className='sidebar-list'>
-      {items.map(item => (
-        <li className={item.active ? 'active' : ''}>
+      {items.map((item, index) => (
+        <li key={index} className={classNames(item.className, { active: item.active })}>
           <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}</i>
           <span>{item.title}</span>
         </li>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -40,6 +40,40 @@ export const Sidebar = () => {
           {
             color: 'pink',
             title: 'Чтение',
+            active: true,
+          },
+        ]}
+        isRemovable
+      />
+      <SidebarList
+        items={[
+          {
+            className: 'sidebar-list__add-button',
+            icon: (
+              <svg
+                width='10'
+                height='10'
+                viewBox='0 0 16 16'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  d='M8 1V15'
+                  stroke='black'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+                <path
+                  d='M1 8H15'
+                  stroke='black'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+            ),
+            title: 'Добавить категорию',
           },
         ]}
       />
